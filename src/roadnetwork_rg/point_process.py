5746eca11741c84faf401dfa6a1084c6149d7d59
sulyi <sulyi.gbox@gmail.com>
2021-05-29 00:37:32 +0200

    Add generator interface to `PointProcess`

--- src/roadnetwork_rg/point_process.py
+++ src/roadnetwork_rg/point_process.py
@@ -1,6 +1,8 @@
-from typing import Union
+from typing import Union, Generator, Iterator, TypeVar
 
 import numpy as np
+
+T = TypeVar('T')
 
 
 class IntensityFunction:
@@ -14,11 +16,15 @@ class IntensityFunction:
 
 
 class PointProcess(object):
-    def __init__(self, rate: Union[int, float, IntensityFunction], size: tuple[int, ...], seed: int) -> None:
-        if not isinstance(size, tuple) or any(not isinstance(dim, int) for dim in size):
+
+    def __init__(self, rate: Union[int, float, IntensityFunction],
+                 size: tuple[int, ...], seed: int) -> None:
+        if (not isinstance(size, tuple) or
+                any(not isinstance(dim, int) for dim in size)):
             raise TypeError("Argument 'size' should be a tuple of integer numbers, not '%s'" % type(size).__name__)
         if any(dim <= 0 for dim in size):
             raise ValueError("Argument 'size' should be positive")
+
         if not isinstance(rate, (int, float, IntensityFunction)):
             raise TypeError("Argument 'rate' should be a number or %s, not '%s'" % (
                 IntensityFunction.__name__, type(rate).__name__))
@@ -30,17 +36,24 @@ class PointProcess(object):
             raise TypeError("Argument 'seed' should be integer number, not '%s'" % type(seed).__name__)
 
         self.rate = rate
-
         self.size = size
-        self._rng = np.random.default_rng(seed)
 
-    def __iter__(self):
+        self._rng = np.random.default_rng(seed)
+        self._next = self._generator()
+
+    def __next__(self) -> Generator[T, None, None]:
+        return next(self._next)
+
+    def __iter__(self) -> Iterator[T]:
+        return self
+
+    def _generator(self) -> Generator[T, None, None]:
         raise NotImplementedError
 
 
 class MarkovChainMonteCarlo(PointProcess):
 
-    def __iter__(self):
+    def _generator(self) -> Generator[tuple[int, ...], None, None]:
         n = self._rng.poisson(self.rate.rate if isinstance(self.rate, IntensityFunction) else self.rate)
         count = 0
         while count <= n:
@@ -59,7 +72,7 @@ class MarkovChainMonteCarlo(PointProcess):
 
 class SpatialPoissonPointProcess(PointProcess):
 
-    def __iter__(self):
+    def _generator(self) -> Generator[tuple[int, ...], None, None]:
         n = self._rng.poisson(self.rate.rate if isinstance(self.rate, IntensityFunction) else self.rate)
         for _ in range(n):
             candidate = tuple(self._rng.integers([0] * len(self.size), self.size))
