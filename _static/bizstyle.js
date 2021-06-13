e72a263b0d2c524113e499a31886fd4aaeac75ca
sulyi <sulyi.gbox@gmail.com>
2021-06-13 21:58:16 +0200

    Rebuild from v0.1.0-0-g136b70a

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("roadnetwork_rg 0.1.0rc1 documentation");
+        $("li.nav-item-0 a").text("roadnetwork_rg 0.1.0 documentation");
     }
 });
